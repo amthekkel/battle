7b660807b9b9fdae59161ebcec7cc3943fc4d812
Abhi Mathews <dev0@10qbit.com>
2021-04-18 10:20:30 +0530

    Game Configuration file

--- App/Config.php
+++ App/Config.php
@@ -5,7 +5,33 @@ namespace App;
 
 class Config{
 
+  const NUM_OF_PLAYERS =2;
+
+  const NUM_OF_ROUNDS = 30;
+
+  const MAX_NAME_LENGTH = 30;
+
+  //Min and Max values for each player properties 
+
+  const MIN_HEALTH =0;
+
+  const MAX_HEALTH =100;
+
+  const MIN_STRENGTH =100;
+
+  const MAX_STRENGTH =100;
+
+  const MIN_DEFENCE =100;
+
+  const MAX_DEFENCE =100;
+
+  const MIN_SPEED =100;
+
+  const MAX_SPEED =100;
+
+  const MIN_LUCK =0;
+
+  const MAX_LUCK =1;
 
 
-	
 }
