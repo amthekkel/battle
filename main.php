61ec335450fb5c90a671d126082bef060324f629
Abhi Mathews <dev0@10qbit.com>
2021-04-20 12:06:08 +0530

    added option to test drawn scenario

--- main.php
+++ main.php
@@ -4,12 +4,14 @@ require './vendor/autoload.php';
 
 use App\Battle;
 
-if(!defined('STDIN')){
-	echo "Please run the application from a command line";
-	exit(0);
+if (!defined('STDIN')) {
+    echo "Please run the application from a command line";
+    exit(0);
 }
 
 
-$game=new Battle;
-// $game=new Battle(5,50);
+$game=new Battle();
+//$game=new Battle(2,1); // To test drawn battle
+$game->start();
+
 exit(1);
